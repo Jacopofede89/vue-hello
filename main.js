8f472cd90aaf59208e9f843a21f30402f0d7d81c
Jacopofede89 <jacopofede89@gmail.com>
2021-11-10 16:04:35 +0100

    finale

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+
+    el: '#myapp',
+    data: {
+    message: 'Benvenuti su VueJS',
+    msg:"color",      
+    immagine: 'https://cdn.statically.io/img/austingil.com/wp-content/uploads/Vue-Blog-Cover.svg',
+    
+    }
+});
+
